47ede6b462b35bc833e1e926c1c13c24c98ca516
hppRC <hpp.ricecake@gmail.com>
2019-08-07 01:25:53 +0900

    finish sample

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import P5Wrapper from 'react-p5-wrapper'
 import testSketch from './testSketch'
 import './App.css'
 
-const App = () => {
-  return (
-    <P5Wrapper sketch={testSketch} rotation={45} />
-  )
+class App extends Component{
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      rotation: 45,
+    }
+  }
+
+	rotationChange(e){
+		this.setState({rotation:e.target.value});
+	}
+
+	render () {
+		return (
+			<div>
+				<P5Wrapper sketch={testSketch} rotation={this.state.rotation} />
+				<input type="range" value={this.state.rotation}  min="0"  max="360" step="1" onInput={this.rotationChange.bind(this)}/>
+			</div>
+		)
+	}
 }
+
 
 export default App
