6a3e47c344b10608e9cae1244c3e0e4df61166d1
hppRC <hpp.ricecake@gmail.com>
2019-08-07 00:46:00 +0900

    add sample

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import P5Wrapper from 'react-p5-wrapper'
+import testSketch from './testSketch'
 import './App.css'
 
 const App = () => {
   return (
-    <h1>Hello!</h1>
+    <P5Wrapper sketch={testSketch} rotation={45} />
   )
 }
 
---
+++ src/testSketch.js
@@ -0,0 +1,26 @@
+const testSketch = (p) => {
+    let rotation = 0
+
+    p.setup = () => {
+        p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL)
+
+    }
+
+    p.myCustomRedrawAccordingToNewPropsHandler = (props) => {
+        if (props.rotation){
+            rotation = props.rotation * Math.PI / 180
+        }
+    }
+
+    p.draw = () => {
+        p.background(100)
+        p.normalMaterial()
+        p.noStroke()
+        p.push()
+        p.rotateY(rotation)
+        p.box(100)
+        p.pop()
+    }
+}
+
+export default testSketch
