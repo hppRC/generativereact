8c346df5599199d3bccc65f6cfc515ec6a26a3c0
hppRC <hpp.ricecake@gmail.com>
2019-10-01 15:12:23 +0900

    🚓あそんだ

--- src/components/pages/works/12.js
+++ src/components/pages/works/12.js
@@ -29,11 +29,11 @@ uniform vec2  uMouse;
 void main() {
 	vec2 uv = vec2( vUv.x * uAspect, vUv.y );
 	vec2 center = vec2( uMouse.x * uAspect, uMouse.y );
-	float radius = 0.2 + sin( uTime ) * 0.025 ;
-	float lightness = radius / length ( uv - vec2(0.5*uAspect, 0.5) - center * 0.5 );
+	float radius = 0.1 + sin( uTime ) * 0.05 ;
+	float lightness = radius / length ( uv - vec2(0.5 * uAspect, 0.5) - center * 0.5 );
 	//lightness = clamp( lightness, 0.0, 1.0 );
 	vec4 color = vec4( vec3( lightness ), 1.0 );
-	color *= vec4( uv, 0.5, 1.0 );
+	color *= vec4( uv, 0.9, 1.0 );
 
 	gl_FragColor = color;
 }
