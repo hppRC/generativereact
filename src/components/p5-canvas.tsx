a9974ca2454cc79ae375bd022226a9ae1bae0b66
hppRC <hpp.ricecake@gmail.com>
2019-12-23 15:07:38 +0900

    add dynamic import and p5.js

--- src/components/p5-canvas.tsx
+++ src/components/p5-canvas.tsx
@@ -1,19 +1,29 @@
-import p5 from 'p5/lib/p5.min';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styled from '@emotion/styled';
+import loadable from '@loadable/component';
+
+const _p5 = loadable.lib(() => import('p5/lib/p5.min'));
 
 type Props = {
   sketch: any;
 };
 
 const P5Canvas: React.FCX<Props> = ({ className, sketch }) => {
+  const [, setP5] = useState();
+  const wrapper = React.createRef<any>();
+
   useEffect(() => {
-    new p5(sketch);
+    if (typeof window !== 'undefined') {
+      _p5.load().then((p5: any) => {
+        setP5(new p5.default(sketch, wrapper.current));
+      });
+    }
   }, [sketch]);
-  return <div className={className} />;
+
+  return <div ref={wrapper} className={className} />;
 };
 
 export const StyledP5Canvas = styled(P5Canvas)``;
 
-export default typeof window !== 'undefined' && StyledP5Canvas;
+export default StyledP5Canvas;
