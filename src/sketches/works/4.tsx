a6a36f6b1b670f9801dd7f2726b7d7ac404f9a7f
hppRC <hpp.ricecake@gmail.com>
2019-12-23 16:42:47 +0900

    chore

--- src/sketches/works/4.tsx
+++ src/sketches/works/4.tsx
@@ -1,55 +1,165 @@
+import p5 from 'p5';
+
 export const sketch = (p: any) => {
-  let agent;
-  let angle = 0;
-  let agents: any = [];
-  const agentCount = 10;
+  let shape: any;
+
+  let joints = 10;
+  let linelength: number;
+  let resolution = 0.9993;
+  let gravity = 5;
+  let damping = 0.99994;
+
+  //let cnt = 0;
 
   p.setup = () => {
-    p.createCanvas(300, 300);
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    linelength = p.windowWidth / 20;
     p.colorMode(p.HSB, 360, 100, 100, 100);
     p.angleMode(p.DEGREES);
-    for (let i = 0; i < agentCount; i++) {
-      agents[i] = new Agent(p.width, p.height, p.random(10));
-    }
+    p.noFill();
+    p.strokeWeight(1);
+    shape = new Shape();
+    p.background(0);
   };
 
   p.draw = () => {
-    p.noStroke();
+    p.background(0, 10);
+    shape.addPos(p.mouseX, p.mouseY);
+    shape.draw();
+    shape.update();
 
-    p.translate(p.width / 2, p.height / 2);
-    p.rotate(angle);
-    for (let i = 0; i < agents.length; i++) {
-      agent = agents[i];
-      p.fill(
-        p.abs(2 * angle + agent.posy) % 256,
-        p.abs(p.sin(agent.posx) - agent.posy + angle) % 256,
-        p.abs(agent.posy - p.abs(255 - angle)) % 256
-      );
-      p.ellipse(agent.posx, agent.posy, agent.radius);
-    }
-    angle += 1.7;
-    if (agents.length < 250) {
-      agents.push(new Agent(p.width, p.height, p.random(10)));
-    }
+    //cnt = (cnt + 0.3) % 360;
   };
 
-  class Agent {
-    private radius: number;
-    private posx: number;
-    private posy: number;
-    private orbitRadius: number;
-    constructor(maxWidth: number, maxHeight: number, radius: number) {
-      this.radius = radius;
-      this.posx = p.random(
-        -(maxWidth / 2 - this.radius * 2) * p.sin(45),
-        (maxWidth / 2 - this.radius * 2) * p.sin(45)
+  class Shape {
+    private shapePath: any;
+    private pendulumPath: any;
+    private iterator: number;
+    private linelength: number;
+    private resolution: number;
+    private pendulum: any;
+    constructor() {
+      this.shapePath = [];
+      this.pendulumPath = [];
+      this.iterator = 0;
+      this.linelength = linelength;
+      this.resolution = resolution;
+      this.pendulum = new Pendulum(this.linelength, joints);
+    }
+
+    addPos(x: number, y: number) {
+      if (x === 0 && y === 0) {
+        return;
+      }
+      const newPos = p.createVector(x, y);
+      this.shapePath.push(newPos);
+    }
+
+    draw() {
+      p.strokeWeight(0.4);
+      if (this.iterator < this.shapePath.length) {
+        const currentIndex = p.floor(this.iterator);
+
+        const currentPos = this.shapePath[currentIndex];
+        const previousPos = this.shapePath[currentIndex - 1];
+        if (previousPos) {
+          const offsetPos: any = p5.Vector.lerp(
+            previousPos,
+            currentPos,
+            this.iterator - currentIndex
+          );
+          const heading =
+            p.atan2(
+              currentPos.y - previousPos.y,
+              currentPos.x - previousPos.x
+            ) - p.HALF_PI;
+
+          p.push();
+          p.translate(offsetPos.x, offsetPos.y);
+          this.pendulum.update(heading);
+          this.pendulum.draw();
+          p.pop();
+
+          this.pendulumPath.push(this.pendulum.getTrail(offsetPos));
+        }
+      }
+    }
+
+    update() {
+      this.iterator += this.resolution;
+      this.iterator = p.constrain(this.iterator, 0, this.shapePath.length);
+    }
+  }
+
+  class Pendulum {
+    private hierarchy: number;
+    private size: number;
+    private pendulumArm: any;
+    private angle: number;
+    private origin: any;
+    private end: any;
+    private gravity: number;
+    private damping: number;
+    private angularAcceleration: number;
+    private angularVelocity: number;
+    constructor(size: number, hierarchy: number) {
+      this.hierarchy = hierarchy - 1;
+      this.size = size;
+      this.pendulumArm =
+        this.hierarchy > 0 ? new Pendulum(this.size / 2, this.hierarchy) : null;
+      this.angle = p.random(p.TAU);
+      this.origin = p.createVector(0, 0);
+      this.end = p.createVector(0, 0);
+      this.gravity = gravity;
+      this.damping = damping;
+      this.angularAcceleration = 0;
+      this.angularVelocity = 0;
+    }
+
+    update(heading: any) {
+      this.end.set(
+        this.origin.x + this.size * p.sin(this.angle),
+        this.origin.y + this.size * p.cos(this.angle)
       );
-      this.posy = p.random(
-        -(maxHeight / 2 - this.radius * 2) * p.sin(45),
-        (maxHeight / 2 - this.radius * 2) * p.sin(45)
-      );
-      this.orbitRadius = p.sqrt(p.pow(this.posx, 2) + p.pow(this.posy, 2));
-      console.log(this.orbitRadius);
+
+      this.angularAcceleration =
+        (-this.gravity / this.size) * p.sin(this.angle + heading);
+      this.angle += this.angularVelocity;
+      this.angularVelocity += this.angularAcceleration;
+      this.angularVelocity *= this.damping;
+
+      if (this.pendulumArm) {
+        this.pendulumArm.update(heading);
+      }
+    }
+
+    getTrail(offset: number, end: any) {
+      if (this.pendulumArm) {
+        if (end) {
+          end.add(this.end);
+        } else {
+          end = this.end.copy();
+        }
+        return this.pendulumArm.getTrail(offset, end);
+      } else {
+        return this.end
+          .copy()
+          .add(end)
+          .add(offset);
+      }
+    }
+
+    draw() {
+      p.noFill();
+      p.stroke(255, 90);
+      p.ellipse(this.origin.x, this.origin.y, this.end.x + this.end.y);
+
+      if (this.pendulumArm) {
+        p.push();
+        p.translate(this.end.x, this.end.y);
+        this.pendulumArm.draw();
+        p.pop();
+      }
     }
   }
 };
